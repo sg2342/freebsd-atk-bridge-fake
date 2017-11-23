99d1e353bfda4d0f2f56077429490f0cedc0883b
Stefan Grundmann <sg2342@googlemail.com>
2017-11-23 04:55:52 +0000

    atk-bridge-fake from sabotage

---
+++ accessibility/atk-bridge-fake/src/atk-bridge.h
@@ -0,0 +1,22 @@
+#ifndef ATK_BRIDGE_H
+#define ATK_BRIDGE_H
+
+#define atk_bridge_adaptor_init(A, B) do{}while(0)
+#define atk_bridge_adaptor_cleanup() do{}while(0)
+
+#if 0
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+int atk_bridge_adaptor_init (int * argc, char ** argv[]);
+void atk_bridge_adaptor_cleanup (void);
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
+#endif
+
+
